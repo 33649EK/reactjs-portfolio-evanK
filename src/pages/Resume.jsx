6568a1b3e5a248c20f0f4ee203994eb41336ddc6
Evan Keller <33649ek@gmail.com>
2024-04-01 16:09:30 -0600

    modified resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,26 +1,9 @@
 const Resume = () => {
   return (
     <div className="resumeContent">
-      <h1 className="resume">Resume</h1>
-      <div>
-        <h2 className="resumeHeader">Education</h2>
-        <ul>
-          <li>University Name - Degree, Year</li>
-          <li>High School Name - Year</li>
-        </ul>
-
-        <h2 className="resumeHeader">Experience</h2>
-        <ul>
-          <li>Company Name - Position, Year</li>
-          <li>Company Name - Position, Year</li>
-        </ul>
-
-        <h2 className="resumeHeader">Skills</h2>
-        <ul>
-          <li>Skill 1</li>
-          <li>Skill 2</li>
-          <li>Skill 3</li>
-        </ul>
+      <h1 className="resume">Resume Placeholder</h1>
+      <div className="resumeImage">
+        <iframe src="./pdf/resumePlaceholder.pdf" width="100%" height="500px"/>
       </div>
     </div>
   );
