0af64c87f2a0a4d959c5f1d32b678f1b6bb9fc24
Evan Keller <336493k@gmail.com>
2024-04-26 14:31:22 -0600

    updated about me paragraph, not complete yet

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,13 +5,12 @@ const About = () => {
       <div className="textContent">
         <h1 className="aboutHeader">About Me</h1>
         <p>
-          Hello there! Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-          ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Hello there! My name is Evan Keller and I am a recent graduate from
+          the University of Denver's full stack coding bootcamp. I am currently
+          searching for a full stack or backend development position. I have
+          experience using React, GraphQL, Mysql, Mongodb, and more. I may not
+          be the most knowledgeable in every area, but I always strive to learn
+          and do better.
         </p>
       </div>
     </div>
