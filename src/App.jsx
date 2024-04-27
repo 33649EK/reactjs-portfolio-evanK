dfa1e0edbca049003301f6c4b3b404f270d3e0dc
Evan Keller <336493k@gmail.com>
2024-04-26 19:11:22 -0600

    imported chakra provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    <>
-      
+    <ChakraProvider>
       <main className="primaryContent">
         <Navbar />
         <Outlet />
         <Footer />
       </main>
-      
-    </>
+    </ChakraProvider>
   );
 }
 
