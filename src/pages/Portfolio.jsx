a8daaedfa5980ea4fbbced041009093e0acaf933
Evan Keller <336493k@gmail.com>
2024-04-28 18:05:42 -0600

    css changes being made

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,26 +7,29 @@ import {
   Image,
   Divider,
   CardFooter,
+  LinkBox,
+  LinkOverlay,
 } from '@chakra-ui/react';
 
-const Project = ({ title, image, altText, link }) => {
+const Project = ({ title, image, altText, link, tech }) => {
   return (
-    <div>
-      {/* <h2>{title}</h2>
-      <a href={link}>
-        <img src={image} alt={altText} />
-      </a> */}
-      <Card maxW="sm">
-        <CardBody>
-          <Image src={image} alt={altText} borderRadius="lg" />
-          <Stack mt="6" spacing="3">
-            <Heading size="md">{title}</Heading>
-          </Stack>
-        </CardBody>
-
-        <CardFooter>Hello</CardFooter>
-      </Card>
-    </div>
+    <>
+      <LinkBox as="article">
+        <Card maxW="sm">
+          <CardBody>
+            <Image src={image} alt={altText} borderRadius="lg" />
+            <Stack mt="6" spacing="3">
+              <Heading size="md">
+                <LinkOverlay href={link} target="_blank">
+                  {title}
+                </LinkOverlay>
+              </Heading>
+            </Stack>
+          </CardBody>
+          <CardFooter>{tech}</CardFooter>
+        </Card>
+      </LinkBox>
+    </>
   );
 };
 
@@ -37,42 +40,47 @@ const Portfolio = () => {
       image: './images/TaskMasterHome.png',
       altText: 'image of task master application',
       link: 'https://github.com/33649EK/Task-Master',
+      tech: 'React/GraphQL/MongoDB',
     },
     {
       title: 'Beat Buddy',
       image: './images/BeatBuddy.png',
       altText: 'image of beat buddy application',
-      link: 'https://github.com/33649EK/beat-buddy/deployments/github-pages',
+      link: 'https://github.com/33649EK/beat-buddy/',
+      tech: 'Javascript/CSS',
     },
     {
       title: 'FurEver Friends',
       image: './images/FurEver.png',
       altText: 'image of fur ever friends application',
       link: 'https://github.com/davisjeff500/FurEver-Friends',
+      tech: 'Handlebars/MySQL',
     },
-    {
-      title: 'Project Title',
-      image: './images/Placeholder.jpg',
-      altText: 'placeholder image',
-      link: 'https://github.com/33649EK',
-    },
-    {
-      title: 'Project Title',
-      image: './images/Placeholder.jpg',
-      altText: 'placeholder image',
-      link: 'https://github.com/33649EK',
-    },
-    {
-      title: 'Project Title',
-      image: './images/Placeholder.jpg',
-      altText: 'placeholder image',
-      link: 'https://github.com/33649EK',
-    },
+    // {
+    //   title: 'Project Title',
+    //   image: './images/Placeholder.jpg',
+    //   altText: 'placeholder image',
+    //   link: 'https://github.com/33649EK',
+    //   tech: 'Placeholder',
+    // },
+    // {
+    //   title: 'Project Title',
+    //   image: './images/Placeholder.jpg',
+    //   altText: 'placeholder image',
+    //   link: 'https://github.com/33649EK',
+    //   tech: 'Placeholder',
+    // },
+    // {
+    //   title: 'Project Title',
+    //   image: './images/Placeholder.jpg',
+    //   altText: 'placeholder image',
+    //   link: 'https://github.com/33649EK',
+    //   tech: 'Placeholder',
+    // },
   ];
 
   return (
     <>
-      <h1>Portfolio</h1>
       <div className="portLinks">
         {projects.map((project, index) => (
           <Project
@@ -81,6 +89,7 @@ const Portfolio = () => {
             image={project.image}
             altText={project.altText}
             link={project.link}
+            tech={project.tech}
           />
         ))}
       </div>
